b0ea55be644f40054687f482bc05cbc13942f3f3
Adeeba Arif <adeebaarif1322@gmail.com>
2024-10-01 09:43:00 +0500

    js working complete

--- script.js
+++ script.js
@@ -9,17 +9,21 @@ calculateAge.onclick = function(){
     var input = document.getElementById('DOB').value
     var DOB = new Date(input)
     var now = new Date()
-    var yearsdiff = now.getFullYear - DOB.getFullYear();
-    var monthsdiff = now.getMonth - DOB.getMonth()
-    var daysdiff = now.getDay() - DOB.getDay()
-    var years = document.getElementById('years').textContent = yearsdiff
-    var months = document.getElementById('months').textContent = monthsdiff
-    var days = document.getElementById('days').textContent = daysdiff
+    var yearsdiff = now.getFullYear() - DOB.getFullYear();
+    var monthsdiff = now.getMonth() - DOB.getMonth()
+    var daysdiff = now.getDate() - DOB.getDate()
+    if (monthsdiff < 0 || (monthsdiff === 0 && daysdiff < 0)) {
+        yearsdiff--;
+        monthsdiff += (monthsdiff < 0) ? 12 : 0;
+    }
+    if (daysdiff < 0) {
+        monthsdiff--;
+        daysdiff += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+    }
+    var years = document.getElementById('years').textContent = "years:" + yearsdiff
+    var months = document.getElementById('months').textContent = "months:" + monthsdiff
+    var days = document.getElementById('days').textContent = "days:" + daysdiff
     
     }
 }
-
-
-
-
 
