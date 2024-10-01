270f7c85ec268035b05334ce8b5924dec63893f7
Adeeba Arif <adeebaarif1322@gmail.com>
2024-10-01 09:25:22 +0500

    add alert to check if the field is empty

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
-var calculateAge = document.getElementsByClassName('button')
-calculateAge.onClick = function(){
-    var input = document.getElementsByClassName('input').value
+var calculateAge = document.getElementById('btn')
+calculateAge.onclick = function(){
+    var input = document.getElementById('DOB').value
+    if(input === ""){
+        alert("Please select date first")
+    }else{
+      
+    var input = document.getElementById('DOB').value
+    var DOB = new Date(input)
     var now = new Date()
-    var ageInMili = now - input
-    console.log(ageInMili)
+    var yearsdiff = now.getFullYear - DOB.getFullYear();
+    var monthsdiff = now.getMonth - DOB.getMonth()
+    var daysdiff = now.getDay() - DOB.getDay()
+    var years = document.getElementById('years').textContent = yearsdiff
+    var months = document.getElementById('months').textContent = monthsdiff
+    var days = document.getElementById('days').textContent = daysdiff
+    
+    }
 }
+
+
+
+
+
